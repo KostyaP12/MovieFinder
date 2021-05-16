781af0c4fcddc12a696779840851b303962a2750
Konstantin <kostyapetrov12rus@gmail.com>
2021-05-16 22:20:50 +0300

    ff

--- app/src/main/java/com/geekbrains/moviefinder/View/MainActivity.kt
+++ app/src/main/java/com/geekbrains/moviefinder/View/MainActivity.kt
@@ -2,6 +2,8 @@ package com.geekbrains.moviefinder.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavAction
 import androidx.navigation.NavHost
 import androidx.navigation.Navigation
@@ -20,11 +22,18 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setNavigation()
+        val mActionBar = findViewById<Toolbar>(R.id.toolbar_actionbar)
+        setSupportActionBar(mActionBar)
     }
 
     private fun setNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
